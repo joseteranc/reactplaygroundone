f0ad4e2e073f145d5ce3ae12057b95c4a2bf3b88
jose teran <joseantonioterancevallos@gmail.com>
2019-09-20 15:50:25 +0200

    badges

---
+++ src/stories/Badge.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Badge from "../componnets/Badge";
+
+export default {
+  title: "Badge"
+};
+
+export const StreetFood = () => <Badge>Street Food</Badge>;
+export const SeeFood = () => <Badge>See Food</Badge>;
+export const Sushi = () => <Badge>Sushi</Badge>;
+export const Wrap = () => <Badge>Wrap</Badge>;
+export const Italienisch = () => <Badge>Italienisch</Badge>;
+export const Asiatisch = () => <Badge>Asiatisch</Badge>;
